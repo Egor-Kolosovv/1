f2a3d29a4ad0f57c5679c7b8ffb3e04ae1b155dd
egor kolosov <e.k0losovkolosov@yandex.ru>
2022-11-26 22:18:09 +0300

    Расширили и добавили main.py в репозиторий dev

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ password_lenght=11
 
 characters = 'abcdefghijklmnopqrstuvwxyz'
 characters =  'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+characters = '1234567890'
 password = ''
 
 for index in range(password_lenght):
